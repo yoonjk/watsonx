25724f0c54303be42f9561aa87a980ded67fa6c3
JG Yun <nexweb1@gmail.com>
2023-08-10 16:59:47 +0900

    Develop fastapi using sqlalchemy

--- ai/app/routers/todos.py
+++ ai/app/routers/todos.py
@@ -20,26 +20,26 @@ def get_db():
     
 db_dependency = Annotated[Session, Depends(get_db)]    
 
-@router.get("/todos")
+@router.get("/todos", tags=['todos'])
 async def read_all(db:db_dependency):
   return db.query(Todos).all()
 
 
-@router.get("/todos/{todo_id}", status_code = status.HTTP_200_OK)
+@router.get("/todos/{todo_id}", tags=['todos'], status_code = status.HTTP_200_OK)
 async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
   todo_model = db.query(Todos).filter(Todos.id == todo_id).first() 
   if todo_model is not None:
     return todo_model 
   raise HTTPException(status_code = 404, detail='Todo not found')
 
-@router.post("/todos", status_code = status.HTTP_201_CREATED)
+@router.post("/todos", tags = ['todos'], status_code = status.HTTP_201_CREATED)
 async def create_todo(db: db_dependency, todo_request: TodoRequest):
   todo_model = Todos(**todo_request.dict())
   
   db.add(todo_model)
   db.commit()
 
-@router.put("/todos/{todo_id}", status_code = status.HTTP_204_NO_CONTENT)
+@router.put("/todos/{todo_id}", tags = ['todos'], status_code = status.HTTP_204_NO_CONTENT)
 async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt = 0)):
   todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
   
@@ -54,7 +54,7 @@ async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int
   db.add(todo_model)
   db.commit()
   
-@router.delete("/todos/{todo_id}", status_code = status.HTTP_204_NO_CONTENT)
+@router.delete("/todos/{todo_id}", tags = ['todos'],  status_code = status.HTTP_204_NO_CONTENT)
 async def delete_todo(db: db_dependency, todo_id: int = Path(gt = 0)):
   todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     
